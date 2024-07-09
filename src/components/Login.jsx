486d5cb85332827dad0deeda7fad7191caf12b83
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-07-10 03:24:11 +0530

    FireBase Integration

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,16 +3,21 @@ import { validateEmail, validatePassword } from "../utils/formValidations";
 
 const Login = () => {
   const [showLoginForm, setshowLoginForm] = useState(true);
+  const [hideInvalidEmailMessage, setHideInvalidEmailMessage] = useState(true);
+  const [hideInvalidPasswordMessage, setHideInvalidPasswordMessage] = useState(true);
 
   const email = useRef();
   const password = useRef();
 
   const validateFields = () => {
     const isEmailValid = validateEmail(email.current.value)
-    modifyTextBoxCss(isEmailValid, email);
-    
     const isPasswordValid = validatePassword(password.current.value);
+    
+    modifyTextBoxCss(isEmailValid, email);
     modifyTextBoxCss(isPasswordValid, password);
+
+    setHideInvalidEmailMessage(isEmailValid);
+    setHideInvalidPasswordMessage(isPasswordValid);
   }
 
   const modifyTextBoxCss = (validationFlag, textboxElem) =>{
@@ -28,7 +33,7 @@ const Login = () => {
   return (
     <div className="login-UI">
       <img
-        className="absolute -z-10 bg-black opacity-45"
+        className="absolute -z-10 bg-gradient-to-b from-black "
         src="https://assets.nflxext.com/ffe/siteui/vlv3/0552717c-9d8c-47bd-9640-4f4efa2de663/537e2c5e-c750-4d4c-9f7a-e66fe93eb977/IN-en-20240701-POP_SIGNUP_TWO_WEEKS-perspective_WEB_b00eeb83-a7e8-4b5b-8ff7-86ed92c51caf_large.jpg"
         alt="bg-img"
       />
@@ -39,7 +44,7 @@ const Login = () => {
       />
       <form
         action=""
-        className="text-white bg-black absolute px-6 py-20 w-1/4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center rounded-2xl opacity-75"
+        className="text-white bg-black absolute px-6 py-20 w-1/4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center rounded-2xl opacity-85"
         onSubmit={(e) => {e.preventDefault()}}
       >
         <h1 className="text-3xl font-bold text-left mx-10 my-9">{showLoginForm ? "Login" : "Sign Up"}</h1>
@@ -55,12 +60,14 @@ const Login = () => {
           ref={email}
           className="p-4 my-3 w-4/5 placeholder:text-white rounded-lg"
         />
+        <h3 className="text-red-600 text-left mx-12 my-0" hidden={hideInvalidEmailMessage}>Invalid Email</h3>
         <input
           type="password"
           ref={password}
           placeholder="Password"
           className="p-4 my-3 w-4/5 placeholder:text-white rounded-lg"
         />
+        <h3 className="text-red-600 text-left mx-12 my-0" hidden={hideInvalidPasswordMessage}>Invalid Password</h3>
         <button className="p-4 my-7 w-4/5 text-white bg-red-600 rounded-lg" onClick={validateFields}>
           {showLoginForm ? "Login" : "Create An Account"}
         </button>
