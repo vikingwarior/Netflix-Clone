3f5093f009a32439c657a3c72e39a88088de6669
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-07-18 02:01:19 +0530

    Adds Header to Browse Component

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Outlet, useNavigate } from 'react-router-dom'
 
 import { handleSignout } from '../utils/authUtils';
+import Header from './Header';
 
 const Browse = () => {
   const navigate = useNavigate();
   return (
     <div>
+      <Header/>
       <button className='' onClick={() => handleSignout(navigate)}>
         Sign Out
       </button>
