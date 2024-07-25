7829f315125ed6976b1b9c3768f693857955a899
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-07-26 00:55:20 +0530

    Moves logo URL to a seperate constants file

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,11 +1,12 @@
 import { useRef, useState } from "react";
+import { useDispatch } from "react-redux";
 
 import CustomMessageBox from "./CustomMessageBox";
 
 import { validateEmail, validatePassword } from "../utils/formValidations";
 import { initiateLogin, initiateSignUp } from "../utils/authUtils";
 
-import { useDispatch } from "react-redux";
+import { LOGO_URL } from "../utils/constants";
 
 const Login = () => {
   const [showLoginForm, setshowLoginForm] = useState(true);
@@ -79,7 +80,7 @@ const Login = () => {
     <div className="login-UI">
       <img
         className="absolute -z-10 bg-gradient-to-b from-black"
-        src="https://assets.nflxext.com/ffe/siteui/vlv3/0552717c-9d8c-47bd-9640-4f4efa2de663/537e2c5e-c750-4d4c-9f7a-e66fe93eb977/IN-en-20240701-POP_SIGNUP_TWO_WEEKS-perspective_WEB_b00eeb83-a7e8-4b5b-8ff7-86ed92c51caf_large.jpg"
+        src={LOGO_URL}
         alt="bg-img"
       />
       <img
