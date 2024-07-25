51da33232c4bfb0f789bdefbd7248f8a4f1af60c
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-07-25 23:07:16 +0530

    Moves all components under children of Wrapper

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,36 +1,30 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
 import Login from "./Login";
 import Browse from "./Browse";
-import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { onAuthStateChanged } from "firebase/auth";
+import Wrapper from "./Wrapper";
 
-import { auth } from "../utils/firebaseConfig";
-import { addUser } from "../utils/redux/userSlice";
+import { useDispatch } from "react-redux";
 
 const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <Login />,
-  },
-  {
-    path: "/browse",
-    element: <Browse />,
+    element: <Wrapper />,
+    Children: [
+      {
+        path: "/",
+        element: <Login />,
+      },
+      {
+        path: "/browse",
+        element: <Browse />,
+      },
+    ],
   },
 ]);
 
 const Body = () => {
-  const dispatch = useDispatch();
-  
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        const { uid, email, displayName } = user;
-        dispatch(addUser({ uid: uid, email: email, displayName: displayName }));
-      }
-    });
-    return () => {};
-  }, []);
+
   return <RouterProvider router={appRouter} />;
 };
 
