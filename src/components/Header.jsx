0823df36215c4a7bdd4c9d42108372adb020e616
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-07-28 14:55:56 +0530

    Fixes JSX issues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { handleSignout } from "../utils/authUtils";
-import { LOGO_URL } from "../utils/constants";
+import { LOGO_URL, USER_IMAGE_URL } from "../utils/constants";
 
 const Header = () => {
   const browseOptions = [
@@ -22,8 +22,8 @@ const Header = () => {
           ))}
         </ul>
       </div>
-      <div>
-        <div id="header-search-div">
+      <div className="flex items-center justify-start">
+        <div id="header-search-div" className="mr-7 cursor-pointer">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -43,30 +43,33 @@ const Header = () => {
             />
           </svg>
         </div>
-        <div id="header-notification-div">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            width={24}
-            height={24}
-            viewBox="0 0 24 24"
-            role="img"
-            data-icon="BellStandard"
-            aria-hidden="true"
-          >
-            <path
-              fillRule="evenodd"
-              clipRule="evenodd"
-              d="M13.0002 4.07092C16.3924 4.55624 19 7.4736 19 11V15.2538C20.0489 15.3307 21.0851 15.4245 22.1072 15.5347L21.8928 17.5232C18.7222 17.1813 15.4092 17 12 17C8.59081 17 5.27788 17.1813 2.10723 17.5232L1.89282 15.5347C2.91498 15.4245 3.95119 15.3307 5.00003 15.2538V11C5.00003 7.47345 7.60784 4.55599 11.0002 4.07086V2H13.0002V4.07092ZM17 15.1287V11C17 8.23858 14.7614 6 12 6C9.2386 6 7.00003 8.23858 7.00003 11V15.1287C8.64066 15.0437 10.3091 15 12 15C13.691 15 15.3594 15.0437 17 15.1287ZM8.62593 19.3712C8.66235 20.5173 10.1512 22 11.9996 22C13.848 22 15.3368 20.5173 15.3732 19.3712C15.3803 19.1489 15.1758 19 14.9533 19H9.0458C8.82333 19 8.61886 19.1489 8.62593 19.3712Z"
-              fill="currentColor"
-            />
-          </svg>
+        <div id="header-notification-div" className="mr-7 cursor-pointer">
+          <button id="menu-button">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              width={24}
+              height={24}
+              viewBox="0 0 24 24"
+              role="img"
+              data-icon="BellStandard"
+              aria-hidden="true"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M13.0002 4.07092C16.3924 4.55624 19 7.4736 19 11V15.2538C20.0489 15.3307 21.0851 15.4245 22.1072 15.5347L21.8928 17.5232C18.7222 17.1813 15.4092 17 12 17C8.59081 17 5.27788 17.1813 2.10723 17.5232L1.89282 15.5347C2.91498 15.4245 3.95119 15.3307 5.00003 15.2538V11C5.00003 7.47345 7.60784 4.55599 11.0002 4.07086V2H13.0002V4.07092ZM17 15.1287V11C17 8.23858 14.7614 6 12 6C9.2386 6 7.00003 8.23858 7.00003 11V15.1287C8.64066 15.0437 10.3091 15 12 15C13.691 15 15.3594 15.0437 17 15.1287ZM8.62593 19.3712C8.66235 20.5173 10.1512 22 11.9996 22C13.848 22 15.3368 20.5173 15.3732 19.3712C15.3803 19.1489 15.1758 19 14.9533 19H9.0458C8.82333 19 8.61886 19.1489 8.62593 19.3712Z"
+                fill="currentColor"
+              />
+            </svg>
+          </button>
         </div>
-        <div id="header-user-menu">
+        <div id="header-user-menu" className="mr-7 cursor-pointer flex">
+          <img src={USER_IMAGE_URL} alt="user-img" className="mr-3 cursor-pointer" />
+          <button className="" onClick={handleSignout}>
+            Sign Out
+          </button>
         </div>
-        <button className="" onClick={handleSignout}>
-          Sign Out
-        </button>
       </div>
     </div>
   );
