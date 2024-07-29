e904bef66b13e803f6d57cd68509815cb370568f
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-07-29 14:57:23 +0530

    Adds a new constant to store user image url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,3 @@
 export const LOGO_URL = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+
+export const USER_IMAGE_URL = "https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABXz4LMjJFidX8MxhZ6qro8PBTjmHbxlaLAbk45W1DXbKsAIOwyHQPiMAuUnF1G24CLi7InJHK4Ge4jkXul1xIW49Dr5S7fc.png?r=e6e";
