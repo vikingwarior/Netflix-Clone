bb4a45ef5f94caa6f720e507e948b15c5dbd3c61
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-08-01 01:01:40 +0530

    Configures app to use TMDB API

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,14 @@
-export const LOGO_URL = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+export const LOGO_URL =
+  "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
 
-export const USER_IMAGE_URL = "https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABXz4LMjJFidX8MxhZ6qro8PBTjmHbxlaLAbk45W1DXbKsAIOwyHQPiMAuUnF1G24CLi7InJHK4Ge4jkXul1xIW49Dr5S7fc.png?r=e6e";
+export const USER_IMAGE_URL =
+  "https://occ-0-2087-2164.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABXz4LMjJFidX8MxhZ6qro8PBTjmHbxlaLAbk45W1DXbKsAIOwyHQPiMAuUnF1G24CLi7InJHK4Ge4jkXul1xIW49Dr5S7fc.png?r=e6e";
+
+export const TMDB_API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1YTg4Y2FkYzdjYzhjMDU0ZGM4OTE3ZGI5MmFmMGZlOSIsIm5iZiI6MTcyMjQ1MjE3NC43MzA3NDYsInN1YiI6IjY2YWE4Mzg3YzFhZmMwZmE4N2JmZTNjYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.sqslciM7o7i2ZRfXW_5wiKT3gZ7xVaHaK-CBPXxMr8k",
+  },
+};
