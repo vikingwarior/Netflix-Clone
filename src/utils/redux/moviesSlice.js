38c027fa0d57b6ad573c7adb88c359b59da67c87
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-08-05 03:10:35 +0530

    Creates a custom hook and store action to handle fetching and storage clips for given movie/show

--- src/utils/redux/moviesSlice.js
+++ src/utils/redux/moviesSlice.js
@@ -4,14 +4,18 @@ const moviesSlice = createSlice({
   name: "nowPlayingMovies",
   initialState: {
     nowPlayingMovies: null,
+    moviePromoClips: null
   },
   reducers: {
     updateMoviesData: (state, action) => {
       state.nowPlayingMovies = action.payload;
     },
+    updatePromoClips: (state, action) => {
+      state.moviePromoClips = action.payload;
+    }
   },
 });
 
-export const { updateMoviesData } = moviesSlice.actions;
+export const { updateMoviesData, updatePromoClips } = moviesSlice.actions;
 
 export default moviesSlice.reducer;
