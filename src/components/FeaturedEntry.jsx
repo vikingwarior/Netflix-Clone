e53b954b71eb0ecb20cee026364539a7302b4300
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-08-13 23:51:12 +0530

    Uses fixed index

--- src/components/FeaturedEntry.jsx
+++ src/components/FeaturedEntry.jsx
@@ -8,25 +8,7 @@ const FeaturedEntry = () => {
   const randomMovieIndex = Math.floor(Math.random() * 20);
   const selectedEntry = useSelector((store) => {
     return store.movies.nowPlayingMovies && store.movies.nowPlayingMovies;
-  })[randomMovieIndex];
-
-  // const selectedEntry = {
-  //   adult: false,
-  //   backdrop_path: "/wNAhuOZ3Zf84jCIlrcI6JhgmY5q.jpg",
-  //   genre_ids: [28, 12, 878],
-  //   id: 786892,
-  //   original_language: "en",
-  //   original_title: "Furiosa: A Mad Max Saga",
-  //   overview:
-  //     "As the world fell, young Furiosa is snatched from the Green Place of Many Mothers and falls into the hands of a great Biker Horde led by the Warlord Dementus. Sweeping through the Wasteland they come across the Citadel presided over by The Immortan Joe. While the two Tyrants war for dominance, Furiosa must survive many trials as she puts together the means to find her way home.",
-  //   popularity: 899.449,
-  //   poster_path: "/iADOJ8Zymht2JPMoy3R7xceZprc.jpg",
-  //   release_date: "2024-05-22",
-  //   title: "Furiosa: A Mad Max Saga",
-  //   video: false,
-  //   vote_average: 7.7,
-  //   vote_count: 2424,
-  // };
+  })[2];
 
   if (selectedEntry === null || selectedEntry === undefined) {
     return;
