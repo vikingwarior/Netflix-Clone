effc4a3dedead32b5f0f4ef94dbd3bf0d06d8c6a
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-08-13 23:51:00 +0530

    Removes hardcoded data

--- src/components/FeaturedEntryTrailer.jsx
+++ src/components/FeaturedEntryTrailer.jsx
@@ -9,19 +9,6 @@ const FeaturedEntryTrailer = ({ entry_id, banner_path }) => {
       return movieClipsObject[0];
     });
 
-  // const randomMovieClip = {
-  //   iso_639_1: "en",
-  //   iso_3166_1: "US",
-  //   name: "Official Trailer #2",
-  //   key: "FVswuip0-co",
-  //   site: "YouTube",
-  //   size: 2160,
-  //   type: "Trailer",
-  //   official: true,
-  //   published_at: "2024-03-19T16:00:19.000Z",
-  //   id: "65f9c571bd990c018622a002",
-  // };
-
   if (randomMovieClip === null || randomMovieClip === undefined) {
     randomMovieClip = {id: "GV3HUDMQ-F8"};
   }
