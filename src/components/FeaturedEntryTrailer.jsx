5a4668a5621470740e9ba6004d68dcdedb21af54
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-08-15 15:31:05 +0530

    Formatting changes

--- src/components/FeaturedEntryTrailer.jsx
+++ src/components/FeaturedEntryTrailer.jsx
@@ -2,22 +2,20 @@ import { useSelector } from "react-redux";
 import useMovieClip from "../utils/hooks/useMovieClip";
 
 const FeaturedEntryTrailer = ({ entry_id, banner_path }) => {
-    useMovieClip(entry_id);
-    let randomMovieClip = useSelector((store) => {
-      const movieClipsObject = store.movies.moviePromoClips;
+  useMovieClip(entry_id);
+  let randomMovieClip = useSelector((store) => store.movies.moviePromoClips);
 
-      return movieClipsObject[0];
-    });
-
-  if (randomMovieClip === null || randomMovieClip === undefined) {
-    randomMovieClip = {id: "GV3HUDMQ-F8"};
-  }
+  !randomMovieClip && (randomMovieClip = { id: "GV3HUDMQ-F8" });
 
   return (
     <div id="trailer-frame">
       <iframe
         title="movie-trailer"
-        src={"https://www.youtube.com/embed/"+ randomMovieClip?.key +"?disablekb=1&cc_load_policy=0&autoplay=1&mute=1"}
+        src={
+          "https://www.youtube.com/embed/" +
+          randomMovieClip?.key +
+          "?disablekb=1&cc_load_policy=0&autoplay=1&mute=1"
+        }
         className="aspect-video w-full pointer-events-none overflow-x-hidden"
       />
     </div>
