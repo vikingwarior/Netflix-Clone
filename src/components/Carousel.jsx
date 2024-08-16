bd9e747b9e9e43420ed2a4a0cdb3a785fbf0fd19
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-08-17 02:14:15 +0530

    Adds carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,11 +1,21 @@
 import CarouselItem from "./CarouselItem";
+
+import { useState } from "react";
 import { useSelector } from "react-redux";
 
 const Carousel = ({ categoryId, categoryName }) => {
   let categoryItemIndex = 0;
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const prev = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? categoryItems.length - 1 : prevIndex - 1));
+  };
+
+  const next = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === categoryItems.length - 1 ? 0 : prevIndex + 1));
+  };
 
   const movies = useSelector((store) => store.movies?.nowPlayingMovies);
-  console.log(movies);
   if (!movies) return;
 
   const categoryItems = movies.filter(({ genre_ids }) =>
@@ -13,20 +23,33 @@ const Carousel = ({ categoryId, categoryName }) => {
   );
 
   return (
-    <div id={categoryName + "-carousel"}>
-      <h1>{categoryName}</h1>
-      <div className="flex">
-        {categoryItems.map(
-          ({ id, backdrop_path, original_title, popularity }) =>
-            categoryItemIndex++ < 5 && (
-              <CarouselItem
-                entryId={id}
-                entryBanner={backdrop_path}
-                entryTitle={original_title}
-                entityRating={popularity}
-              />
-            )
-        )}
+    <div id={`${categoryName}-carousel`} className="carousel-container">
+      <div className="flex items-center">
+        <h1 className="mx-4">{categoryName}</h1>
+        <div>
+          <button className="mx-2" onClick={prev}>
+            Left
+          </button>
+          <button className="mx-2" onClick={next}>
+            Right
+          </button>
+        </div>
+      </div>
+      <div
+        className="flex overflow-hidden transition-transform ease-out duration-500"
+        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+      >
+        {categoryItems
+          .slice(0, 5)
+          .map(({ id, backdrop_path, original_title, popularity }) => (
+            <CarouselItem
+              key={id}
+              entryId={id}
+              entryBanner={backdrop_path}
+              entryTitle={original_title}
+              entityRating={popularity}
+            />
+          ))}
       </div>
     </div>
   );
--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,9 +1,11 @@
 import { TMDB_IMAGE_API } from "../utils/constants";
 
-
-const CarouselItem = ({entryId, entryTitle, entityRating, entryBanner}) => {
-
-  return <div><img src={TMDB_IMAGE_API + entryBanner} alt="" /></div>;
+const CarouselItem = ({ entryId, entryTitle, entityRating, entryBanner }) => {
+  return (
+    <div className="w-96  mr-9 shrink-0">
+      <img src={TMDB_IMAGE_API + entryBanner} alt="" />
+    </div>
+  );
 };
 
 export default CarouselItem;
