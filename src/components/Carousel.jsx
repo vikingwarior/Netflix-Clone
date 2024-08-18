cef4caee1a332f6057b9fd1835c225700052d7b9
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-08-19 00:31:41 +0530

    New logic and look feel for Nav buttons

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,4 +1,5 @@
 import CarouselItem from "./CarouselItem";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 import { useState } from "react";
 import { useSelector } from "react-redux";
@@ -6,49 +7,64 @@ import { useSelector } from "react-redux";
 const Carousel = ({ categoryId, categoryName }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const prev = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === 0 ? categoryItems.length - 1 : prevIndex - 1));
-  };
-
-  const next = () => {
-    setCurrentIndex((prevIndex) => (prevIndex === categoryItems.length - 1 ? 0 : prevIndex + 1));
-  };
-
   const movies = useSelector((store) => store.movies?.nowPlayingMovies);
-  if (!movies) return;
+  if (!movies) return null;
 
   const categoryItems = movies.filter(({ genre_ids }) =>
     genre_ids.includes(categoryId)
   );
 
+  const handlePrevClick = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? categoryItems.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNextClick = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === categoryItems.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
   return (
-    <div id={`${categoryName}-carousel`} className="carousel-container">
+    <div
+      id={`${categoryName}-carousel`}
+      className="carousel-container relative"
+    >
       <div className="flex items-center justify-between">
         <h1 className="mr-4 py-4 text-3xl font-bold">{categoryName}</h1>
         <div>
-          <button className="mx-2" onClick={prev}>
-            Left
+          <button className="mx-2" onClick={handlePrevClick}>
+            <FaArrowLeft />
           </button>
-          <button className="mx-2" onClick={next}>
-            Right
+          <button className="mx-2" onClick={handleNextClick}>
+            <FaArrowRight />
           </button>
         </div>
       </div>
-      <div
-        className="flex overflow-hidden transition-transform ease-out duration-500"
-        style={{ transform: `translateX(-${currentIndex * 100}%)` }}
-      >
-        {categoryItems
-          .slice(0, 5)
-          .map(({ id, backdrop_path, original_title, popularity }) => (
-            <CarouselItem
-              key={id}
-              entryId={id}
-              entryBanner={backdrop_path}
-              entryTitle={original_title}
-              entityRating={popularity}
-            />
-          ))}
+      <div className="overflow-hidden">
+        <div
+          className="flex transition-transform ease-out duration-500"
+          style={{
+            transform: `translateX(-${
+              currentIndex * (1000 / categoryItems.length)
+            }%)`,
+          }}
+        >
+          {categoryItems.map(
+            ({ id, backdrop_path, original_title, popularity }, index) => (
+              <div key={index} className="carousel-entry flex-shrink-0 w-1/5">
+                <CarouselItem
+                  key={id}
+                  entryId={id}
+                  entryBanner={backdrop_path}
+                  entryTitle={original_title}
+                  entityRating={popularity}
+                />
+              </div>
+            )
+          )}
+        </div>
       </div>
     </div>
   );
