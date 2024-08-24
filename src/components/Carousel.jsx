8a16a4b8f4c655635d4ec6987bacf00cf33f5f08
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-08-25 00:22:09 +0530

    Adds the functionality to hide navigation functionality in the carousel component

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -3,18 +3,22 @@ import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 
 import { useState } from "react";
 
-const Carousel = ({ carouselTitle, displayItems }) => {
+const Carousel = ({
+  carouselTitle,
+  displayItems,
+  showNavigationBtns = true,
+}) => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handlePrevClick = () => {
     setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? (displayItems.length / 4) - 1 : prevIndex - 1
+      prevIndex === 0 ? displayItems.length / 4 - 1 : prevIndex - 1
     );
   };
 
   const handleNextClick = () => {
     setCurrentIndex((prevIndex) =>
-      prevIndex === (displayItems.length / 4) - 1 ? 0 : prevIndex + 1
+      prevIndex === displayItems.length / 4 - 1 ? 0 : prevIndex + 1
     );
   };
 
@@ -25,14 +29,16 @@ const Carousel = ({ carouselTitle, displayItems }) => {
     >
       <div className="flex items-center justify-between">
         <h1 className="mr-4 py-4 text-3xl font-bold">{carouselTitle}</h1>
-        <div>
-          <button className="mx-2" onClick={handlePrevClick}>
-            <FaArrowLeft />
-          </button>
-          <button className="mx-2" onClick={handleNextClick}>
-            <FaArrowRight />
-          </button>
-        </div>
+        {showNavigationBtns && (
+          <div>
+            <button className="mx-2" onClick={handlePrevClick}>
+              <FaArrowLeft />
+            </button>
+            <button className="mx-2" onClick={handleNextClick}>
+              <FaArrowRight />
+            </button>
+          </div>
+        )}
       </div>
       <div className="overflow-hidden">
         <div
@@ -44,13 +50,13 @@ const Carousel = ({ carouselTitle, displayItems }) => {
           }}
         >
           {displayItems.map(
-            ({ id, backdrop_path, original_title, popularity }, index) => (
+            ({ id, backdrop_path, title, popularity }, index) => (
               <div key={index} className="carousel-entry flex-shrink-0 w-1/5">
                 <CarouselItem
                   key={id}
                   entryId={id}
                   entryBanner={backdrop_path}
-                  entryTitle={original_title}
+                  entryTitle={title}
                   entityRating={popularity}
                 />
               </div>
