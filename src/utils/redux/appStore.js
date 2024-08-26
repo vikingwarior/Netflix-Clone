7c3e5a83ba9e25efd1f6d73ad5e3cf0d9c6df5cf
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-08-26 22:06:49 +0530

    Adds recommendation slice to store

--- src/utils/redux/appStore.js
+++ src/utils/redux/appStore.js
@@ -1,12 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userSliceReducer from "./userSlice"
+
+import userSliceReducer from "./userSlice";
 import moviesSliceReducer from "./moviesSlice";
+import recommendationsSliceReducer from "./recommendationsSlice";
 
 const appStore = configureStore({
-    reducer : {
-        user : userSliceReducer,
-        movies : moviesSliceReducer
-    }
+  reducer: {
+    user: userSliceReducer,
+    movies: moviesSliceReducer,
+    recommendations: recommendationsSliceReducer,
+  },
 });
 
 export default appStore;
