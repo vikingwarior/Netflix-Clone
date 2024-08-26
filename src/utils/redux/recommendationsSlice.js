7d82725f2f4d01c24d2da9382a559f8b8ed04115
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-08-26 22:06:28 +0530

    Minor reference correction

--- src/utils/redux/recommendationsSlice.js
+++ src/utils/redux/recommendationsSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const recommendationsSlice = createSlice({
-    name: recommendations,
+    name: "recommendations",
     initialState: {
         movieRecommendations: []
     },
