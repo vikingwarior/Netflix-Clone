9be1c45b47c1c696ac05228612e555b607a8ef70
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-08-26 21:03:01 +0530

    Adds a new slice to store recommendations component data

---
+++ src/utils/redux/recommendationsSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const recommendationsSlice = createSlice({
+    name: recommendations,
+    initialState: {
+        movieRecommendations: []
+    },
+    reducers:{
+        addOrUpdateRecommendations: (state, action) => {
+            state.movieRecommendations = action.payload;
+        },
+        clearSlice: (state) => {
+            state.movieRecommendations.length = 0;
+        }
+    }
+});
+
+export const {addOrUpdateRecommendations, clearSlice} = recommendationsSlice.actions;
+
+export default recommendationsSlice.reducer;
