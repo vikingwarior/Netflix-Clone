e16f3a78548f03e18254d501b40a5da3950c8c15
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-08-28 22:49:32 +0530

    implements logic to get Tmdb response

--- src/components/RecommendationContainer.jsx
+++ src/components/RecommendationContainer.jsx
@@ -1,34 +1,54 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import GeminiUtils from "../utils/GeminiUtils";
 
 import Carousel from "./Carousel";
 
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addOrUpdateRecommendations } from "../utils/redux/recommendationsSlice";
 
-import { GEMINI_KEY } from "../utils/constants";
+import { GEMINI_KEY, TMDB_API_OPTIONS } from "../utils/constants";
 
 const RecommendationContainer = () => {
-  const genAI = new GoogleGenerativeAI(GEMINI_KEY);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const geminiObject = new GeminiUtils(GEMINI_KEY);
 
   const dispatch = useDispatch();
   const searchQuery = useRef(null);
-  // const [displayItemsArray, setDisplayItemsArray] = useState([]);
-  const displayItemsArray = useSelector(store => store.recommendations.movieRecommendations);
+
+  const displayItemsArray = useSelector(
+    (store) => store.recommendations.movieRecommendations
+  );
 
   const showSuggestions = async () => {
+    // const suggestions = await getSuggestionsArray();
+    const suggestions = ["Hera Pheri", "Golmaal", "Welcome", "Housefull"];
+
+    const searchResponsesArray = await Promise.all(
+      suggestions.map(
+        async (movieName) => await getMovieSearchResults(movieName)
+      )
+    );
+
+    console.log(searchResponsesArray);
+  };
+
+  const getSuggestionsArray = async () => {
     const finalPrompt = `Suggest names of movies that match the query: ${searchQuery.current.value}. Return Strings as comma seperated values, where each entry is a movie name. Example: "Movie1Name, Movie2Name, Movie3Name,Movie4Name, Movie5Name". Only send the response and nothing else.`;
 
-    const result = await model.generateContent(finalPrompt);
-    const response = result.response;
-    const text = await response.text();
+    const geminiResponse = await geminiObject.getGeminiResponse(finalPrompt);
 
-    const finalData = text.split(",", 5);
-    console.log(finalData);
-
-    dispatch(addOrUpdateRecommendations(finalData));
+    return geminiResponse?.split(",", 5);
   };
+
+  const getMovieSearchResults = async (movieName) => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?query=${movieName}&include_adult=true&language=en-US&page=1`,
+      TMDB_API_OPTIONS
+    );
+    const json = await response?.json();
+    return json.results;
+  };
+
+  useEffect(() => {}, []);
 
   return (
     <div className="w-full h-screen bg-black/30 backdrop-blur-lg pt-36 text-white">
@@ -52,14 +72,14 @@ const RecommendationContainer = () => {
         </button>
       </form>
       <div id="recommendation-result-carousels" className="px-8">
-        {displayItemsArray.length > 0 &&
+        {/* {displayItemsArray.length > 0 &&
           displayItemsArray.map((displayItemsArrayElem) => (
             <Carousel
               carouselTitle="test"
               displayItems={displayItemsArrayElem}
               showNavigationBtns={false}
             />
-          ))}
+          ))} */}
       </div>
     </div>
   );
