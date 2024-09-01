c4ffec5596a9505b112262c658376eb12c545554
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-09-01 23:54:57 +0530

    Makes spotlight video more visible

--- src/components/BrowseCarouselHolder.jsx
+++ src/components/BrowseCarouselHolder.jsx
@@ -18,7 +18,7 @@ const BrowseCarouselHolder = () => {
   if (!movies) return null;
 
   return (
-    <div id="bottom-container" className="relative z-10 -mt-80 px-8">
+    <div id="bottom-container" className="relative z-10 -mt-72 px-8">
       {GENRE_IDs.map(({ id, name }) => {
         const moviesToDisplay = movies.filter(({ genre_ids }) =>
           genre_ids.includes(id)
